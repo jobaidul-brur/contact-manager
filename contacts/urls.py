582639d6b48bb5ef4e04a91e8af6ea131742dad2
Md. Jobaidul Islam <jobaidul.cse.brur@gmail.com>
2023-07-21 04:42:07 +0600

    add single api end point

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import ContactList
+from .views import ContactList, ContactDetail, api_root
 
 urlpatterns = [
-    path("contacts/", ContactList.as_view(), name="contact-list-create"),
+    path("contacts/", ContactList.as_view(), name="contact-list"),
+    path("contacts/<int:pk>", ContactDetail.as_view(), name="contact-detail"),
+    path("", api_root),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,8 +1,25 @@
 from rest_framework import generics
 from .models import Contact
 from .serializers import ContactSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 
 class ContactList(generics.ListCreateAPIView):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
+
+
+class ContactDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+
+
+@api_view(["GET"])
+def api_root(request, format=None):
+    return Response(
+        {
+            "contacts": reverse("contact-list", request=request, format=format),
+        }
+    )
