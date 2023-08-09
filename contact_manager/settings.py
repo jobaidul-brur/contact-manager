7743c0a9edc2ee8c24ca679322523831bcdf13dd
Md. Jobaidul Islam <jobaidul.cse.brur@gmail.com>
2023-08-10 03:14:16 +0600

    CORS_ALLOW_ALL_ORIGINS

--- contact_manager/settings.py
+++ contact_manager/settings.py
@@ -131,8 +131,8 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-# CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://localhost:3000",
-]
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     "https://localhost:5173",
+#     "https://localhost:3000",
+# ]
